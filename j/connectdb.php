f2bf18956ca2694e45108c11eb132002552260cf
66010914155-cell <66010914155@msu.ac.th>
2026-02-17 12:37:00 +0700

    Update database password in connectdb.php

--- j/connectdb.php
+++ j/connectdb.php
@@ -1,8 +1,9 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pw = "";            
+$pw = "chinnapat";            
 $db = "4155db";
 $conn = mysqli_connect($host, $user, $pw, $db) or die ("Error: " . mysqli_connect_error());
 mysqli_query($conn, "SET NAMES utf8");
+
 ?>
